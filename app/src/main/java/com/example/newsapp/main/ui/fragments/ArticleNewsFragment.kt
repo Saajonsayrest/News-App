7ad7f19251f3dc018396f46d56fbbdcfd399cd6c
Saajonsayrest <sajonshrestha5@gmail.com>
2023-11-22 16:26:59 +0545

    fix loading issue in web view

--- app/src/main/java/com/example/newsapp/main/ui/fragments/ArticleNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/main/ui/fragments/ArticleNewsFragment.kt
@@ -3,6 +3,8 @@ package com.example.newsapp.main.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -17,25 +19,42 @@ import kotlinx.coroutines.launch
 
 class ArticleNewsFragment : Fragment(R.layout.fragment_article_news) {
 
-
-    lateinit var viewModel: NewsViewModel
+    private lateinit var viewModel: NewsViewModel
 
     private var _binding: FragmentArticleNewsBinding? = null
     private val binding get() = _binding!!
     private var job: Job? = null
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+
+        _binding = FragmentArticleNewsBinding.bind(view)
+
+        // Show loading indicator while the WebView is loading
+        binding.progressBar.visibility = View.VISIBLE
+
         job = viewLifecycleOwner.lifecycleScope.launch {
             _binding = FragmentArticleNewsBinding.bind(view)
             binding.webView.apply {
                 webViewClient = WebViewClient()
+
+                // Use WebChromeClient to track loading progress
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                        super.onProgressChanged(view, newProgress)
+
+                        // Hide the loading indicator when the page is fully loaded
+                        if (newProgress == 100) {
+                            binding.progressBar.visibility = View.GONE
+                        }
+                    }
+                }
+
                 val article = arguments?.getParcelable<Article>("article")
                 article?.url?.let {
                     Log.d("abcd", it)
-                    loadUrl(article.url)
+                    loadUrl(it)
                 }
             }
         }
